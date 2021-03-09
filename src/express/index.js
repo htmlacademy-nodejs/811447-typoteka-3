35ae98153ed8d908336f69261adf76201fd4bcb1
Anastasiya <v.anastasiya26@mail.ru>
2021-03-09 17:20:03 +0700

    task 16

--- src/express/index.js
+++ src/express/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 
 const articlesRoutes = require(`./routes/articles-routes`);
@@ -7,10 +8,15 @@ const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 const app = express();
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
