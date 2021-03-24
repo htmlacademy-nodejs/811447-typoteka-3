d273f059c6461f1209cefefce68a0d9f470a2ac8
Anastasiya <v.anastasiya26@mail.ru>
2021-03-24 15:36:24 +0700

    task 17

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,30 +2,11 @@
 
 const express = require(`express`);
 const chalk = require(`chalk`);
-// const http = require(`http`);
 const fs = require(`fs`).promises;
 const {HttpCode, ExitCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
-
-// const sendResponse = (res, statusCode, message) => {
-//   const template = `
-//     <!Doctype html>
-//       <html lang="ru">
-//       <head>
-//         <title>With love from Node</title>
-//       </head>
-//       <body>${message}</body>
-//     </html>`.trim();
-
-//   res.statusCode = statusCode;
-//   res.writeHead(statusCode, {
-//     'Content-Type': `text/html; charset=UTF-8`,
-//   });
-
-//   res.end(template);
-// };
 
 const app = express();
 app.use(express.json());
@@ -45,27 +26,6 @@ app.use((req, res) => res
   .status(HttpCode.NOT_FOUND)
   .send(`Not found`));
 
-// const onClientConnect = async (req, res) => {
-//   const notFoundMessageText = `Not found`;
-
-//   switch (req.url) {
-//     case `/`:
-//       try {
-//         const fileContent = await fs.readFile(FILENAME);
-//         const mocks = JSON.parse(fileContent);
-//         const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-//         sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-//       } catch (err) {
-//         sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-//       }
-
-//       break;
-//     default:
-//       sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-//       break;
-//   }
-// };
-
 module.exports = {
   name: `--server`,
   run(args) {
@@ -84,15 +44,5 @@ module.exports = {
       console.error(`Ошибка при создании сервера`, err);
       process.exit(ExitCode.ERROR);
     }
-
-    // http.createServer(onClientConnect)
-    // .listen(port)
-    // .on(`listening`, (err) => {
-    //   if (err) {
-    //     return console.error(`Ошибка при создании сервера`, err);
-    //   }
-
-    //   return console.info(chalk.green(`Ожидаю соединений на ${port} порту`));
-    // });
   }
 };
