9b1226d02ab1a95b6a167fcfd4a0f8fb7bd3f400
Anastasiya <v.anastasiya26@mail.ru>
2021-03-24 15:35:37 +0700

    task 17

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,51 +1,70 @@
 'use strict';
 
+const express = require(`express`);
 const chalk = require(`chalk`);
-const http = require(`http`);
+// const http = require(`http`);
 const fs = require(`fs`).promises;
-const {HttpCode} = require(`../../constants`);
+const {HttpCode, ExitCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
+// const sendResponse = (res, statusCode, message) => {
+//   const template = `
+//     <!Doctype html>
+//       <html lang="ru">
+//       <head>
+//         <title>With love from Node</title>
+//       </head>
+//       <body>${message}</body>
+//     </html>`.trim();
 
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
+//   res.statusCode = statusCode;
+//   res.writeHead(statusCode, {
+//     'Content-Type': `text/html; charset=UTF-8`,
+//   });
 
-  res.end(template);
-};
+//   res.end(template);
+// };
 
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
+const app = express();
+app.use(express.json());
 
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILENAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILENAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.json([]);
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
   }
-};
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
+
+// const onClientConnect = async (req, res) => {
+//   const notFoundMessageText = `Not found`;
+
+//   switch (req.url) {
+//     case `/`:
+//       try {
+//         const fileContent = await fs.readFile(FILENAME);
+//         const mocks = JSON.parse(fileContent);
+//         const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+//         sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+//       } catch (err) {
+//         sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+//       }
+
+//       break;
+//     default:
+//       sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+//       break;
+//   }
+// };
 
 module.exports = {
   name: `--server`,
@@ -53,14 +72,27 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, (err) => {
-      if (err) {
-        return console.error(`Ошибка при создании сервера`, err);
-      }
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return console.error(`Ошибка при создании сервера`, err);
+        }
 
-      return console.info(chalk.green(`Ожидаю соединений на ${port} порту`));
-    });
+        return console.info(chalk.green(`Ожидаю соединений на ${port} порту`));
+      });
+    } catch (err) {
+      console.error(`Ошибка при создании сервера`, err);
+      process.exit(ExitCode.ERROR);
+    }
+
+    // http.createServer(onClientConnect)
+    // .listen(port)
+    // .on(`listening`, (err) => {
+    //   if (err) {
+    //     return console.error(`Ошибка при создании сервера`, err);
+    //   }
+
+    //   return console.info(chalk.green(`Ожидаю соединений на ${port} порту`));
+    // });
   }
 };
