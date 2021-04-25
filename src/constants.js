fbe3eecefd2da3bb1b2eca8b9c045056499ba0ed
Anastasiya <v.anastasiya26@mail.ru>
2021-04-25 15:29:36 +0700

    task 21

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@
 module.exports.DEFAULT_COMMAND = `--help`;
 
 module.exports.USER_ARGV_INDEX = 2;
+module.exports.MAX_ID_LENGTH = 6;
 
 module.exports.ExitCode = {
   ERROR: 1,
@@ -11,8 +12,10 @@ module.exports.ExitCode = {
 
 module.exports.HttpCode = {
   OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
-  FORBIDDEN: 403,
-  UNAUTHORIZED: 401,
 };
